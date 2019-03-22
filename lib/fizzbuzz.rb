b0249bee5a2dcdb2b0d1c51aefe14cd2da077fa9
Dominic White <your email>
2019-03-22 13:14:48 +0000

    return Fizz when passed a number divisible by 3

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizz_buzz(number)
-     if number == 3
-       return "Fizz"
-     elsif number == 5
-       return "Buzz"
-     elsif number % 15 == 0
-       return "FizzBuzz"
-     end
-     number
+  if number % 15 == 0
+    return "FizzBuzz"
+  elsif number == 5
+    return "Buzz"
+  elsif number % 3 == 0
+    return "Fizz"
+  end
+  number
 end 
